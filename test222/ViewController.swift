cab2ca24a12230746cb5cddf407fda40e0050f4d
Alexander Makarov <magrus87@gmail.com>
2021-02-24 00:27:43 +0400

    Initial Commit

---
+++ test222/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test222
+//
+//  Created by Alexander Makarov on 24.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
